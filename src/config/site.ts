0b0751742fab8f6be92c9e69f29282c540677f76
Maroyan Putros <maroyanp@gmail.com>
2026-02-28 01:10:20 -0500

    removed social links

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,23 +2,22 @@ import ogImage from "../assets/og-image.png";
 
 export const siteConfig = {
   name: "Reliable Home Appliance Service",
-  description: "Applaince Repair in Windsor, Ontario. Appliance Parts, Windsor, Ontario",
+  description: "Applaince Service and Appliance Repair in all of Essex county",
   url: "https://appliancerepairpro.netlify.app/",
   lang: "en",
   locale: "en_US",
-  author: "Maroyan Putros",
+  author: "MP Web Design",
   twitter: "na",
   ogImage: ogImage,
-  socialLinks: {
-    twitter: "https:/git /twitter.com",
-    github: "",
-    discord: "https://discord.com",
-  },
+  // socialLinks: {
+  //   twitter: "https:/git /twitter.com",
+  //   github: "",
+  //   discord: "https://discord.com",
+  // },
   navLinks: [
     { text: "Home", href: "/" },
     { text: "About", href: "/about" },
     { text: "Services", href: "/services" },
     { text: "Contact", href: "/contact" },
-    { text: "Widgets", href: "/widgets" },
   ],
 };
